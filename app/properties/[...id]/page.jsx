11634775f53be4597a35260fa28a3c81c0c23f2e
ZobairHussain <zobair.hu@gmail.com>
2024-12-13 18:10:50 +0600

    file based routing

---
+++ app/properties/[...id]/page.jsx
@@ -0,0 +1,8 @@
+'use client';
+import { useRouter } from "next/navigation";
+const PropertyPage = () => {
+    console.log('Property page component')
+    return ( <div>Property Page</div> );
+}
+ 
+export default PropertyPage;
